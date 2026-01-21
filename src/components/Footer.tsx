2f70931697f08ee4ad77fc48eaabb11368437009
Maxence Rossignol <maxence.rossignol@student-cs.fr>
2026-01-21 20:53:46 +0100

    upd(footer):fixed the update of the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,17 @@
 const Footer = () => {
   return (
-    <footer className="w-full py-12 mt-20 border-t border-gray-200 dark:border-gray-800">
+    <footer className="w-full py-10 mt-20 border-t border-gray-200 dark:border-gray-800">
       <div className="container mx-auto text-center">
         
         {/* The "Drop a line" Section */}
         <p className="text-lg font-medium text-gray-900 dark:text-gray-300">
-          Have a question or just want to say hi?
+          Want to chat about anything? <a 
+            href="mailto:contact@maxencerossignol.com" 
+            className="inline-block text-lg text-gray-900 dark:text-gray-300 hover:text-blue-800 dark:hover:text-blue-300 transition-colors border-b-2 border-gray-900 dark:border-gray-300 hover:border-blue-800 dark:hover:border-blue-300"
+          >
+            Contact me
+          </a>
         </p>
-        <a 
-          href="mailto:contact@maxencerossignol.com" 
-          className="inline-block mt-2 text-xl text-blue-600 dark:text-blue-400 hover:text-blue-800 dark:hover:text-blue-300 transition-colors border-b-2 border-blue-600 dark:border-blue-400 hover:border-blue-800 dark:hover:border-blue-300"
-        >
-          Drop me a line
-        </a>
 
         {/* The Copyright Section */}
         <div className="mt-8 text-sm text-gray-500 dark:text-gray-400">
